b6de9edde2c2c74b8c6d06b9106ff006546f2fd5
bamchoh <bamchoh@gmail.com>
2019-01-27 03:02:43 +0900

    Add a test for Rect

--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,13 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 
 import { Rect } from "./components/Rect";
+import { CheckboxWithLabel} from "./components/CheckboxWithLabel";
 
 ReactDOM.render(
 	<div>
 		<Rect num={1} bgcolor="#00FF00" />
 		<Rect num={2} bgcolor="#FF0000" />
+		<CheckboxWithLabel labelOn="On" labelOff="Off" />
 	</div>,
 	document.getElementById("root")
 );
