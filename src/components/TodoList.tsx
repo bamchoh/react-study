8732f6d4516b3f973f5e77e4c6da3ae14bf6b5cf
bamchoh <bamchoh@gmail.com>
2019-02-22 01:05:37 +0900

    First coding for dnd

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,4 +1,7 @@
 import * as React from 'react';
+import { DragDropContext } from 'react-beautiful-dnd';
+import { Droppable } from 'react-beautiful-dnd';
+import { Draggable } from 'react-beautiful-dnd';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
@@ -17,6 +20,45 @@ interface PropsWithDispatch {
 
 const TodoList = withStyles(styles)(
   class extends React.Component<PropsWithDispatch & WithStyles<typeof styles>, {}> {
+    onDragEnd = (result:any) => {
+      const {destination, source ,draggableId} = result;
+
+      if(!destination) {
+        return;
+      }
+
+      if (
+        destination.droppableId === source.droppableId &&
+        destination.index === source.index
+      ) {
+        return;
+      }
+
+      console.log(draggableId)
+      console.log(destination.index)
+      console.log(source.index)
+
+      // const column = this.state.columns[source.droppableId];
+      // const newTaskIds = Array.from(column.taskIds);
+      // newTaskIds.splice(source.index, 1);
+      // newTaskIds.splice(destination.index, 0, draggableId);
+
+      // const newColumn = {
+      //   ...column,
+      //   taskIds: newTaskIds,
+      // };
+
+      // const newState = {
+      //   ...this.state,
+      //   columns: {
+      //     ...this.state.columns,
+      //     [newColumn.id]: newColumn,
+      //   },
+      // };
+
+      // this.setState(newState);
+    }
+
     on_click_li = (e: React.MouseEvent<HTMLDivElement>) => {
       var id:string = e.currentTarget.id
       this.props.action.completeTodo({id: id});
@@ -35,29 +77,44 @@ const TodoList = withStyles(styles)(
     }
 
     listItems = () => {
-      return this.props.todos.map((item:TodoState) => {
-        return(
-          <ListItem key={item.id} button id={String(item.id)} onClick={this.on_click_li}>
-            <ListItemText>
-              {this.drawItems(item)}
-            </ListItemText>
-            <ListItemSecondaryAction>
-              <IconButton aria-label="Delete" onClick={this.on_click_for_del} id={String(item.id)}>
-                <DeleteIcon />
-              </IconButton>
-            </ListItemSecondaryAction>
-          </ListItem>
-        )
-      });
+      return this.props.todos.map((item:TodoState, index:number) => (
+        <div key={item.id}>
+        <Draggable draggableId={item.id} index={index}>
+        {(provided:any) => (
+          <div {...provided.draggableProps} {...provided.dragHandleProps} ref={provided.innerRef} >
+            <ListItem key={item.id} button id={String(item.id)} onClick={this.on_click_li}>
+              <ListItemText>
+                {this.drawItems(item)}
+              </ListItemText>
+              <ListItemSecondaryAction>
+                <IconButton aria-label="Delete" onClick={this.on_click_for_del} id={String(item.id)}>
+                  <DeleteIcon />
+                </IconButton>
+              </ListItemSecondaryAction>
+            </ListItem>
+          </div>
+        )}
+        </Draggable>
+        </div>
+      ));
     }
 
     render() {
       const { user } = this.props
       if(user.init && user.uid !== "") {
         return (
-          <List>
-          {this.listItems()}
-          </List>
+          <DragDropContext onDragEnd={this.onDragEnd}>
+            <List>
+              <Droppable droppableId="columns-1">
+                {(provided:any) => (
+                  <div ref={provided.innerRef} {...provided.droppableProps} >
+                    {this.listItems()}
+                    {provided.placeholder}
+                  </div>
+                )}
+              </Droppable>
+            </List>
+          </DragDropContext>
         )
       }
 
